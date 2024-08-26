f3d3d4bfe3a1ab50712e9953b81af86607b9cf88
Dinn <dinn@richalien.net>
2024-08-26 18:24:24 +0900

    LocalDatabase 코드 최적화

--- MVP/Container/MvpContainer.cs
+++ MVP/Container/MvpContainer.cs
@@ -2,18 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Newtonsoft.Json;
-using Redbean.Security;
 using Redbean.MVP;
-using UnityEngine;
 
 namespace Redbean.Singleton
 {
 	public class MvpContainer : ISingletonContainer
 	{
-		public const string PLAYER_PREFS_KEY = "PLAYER_PREFS_DATA_GROUP";
-		
-		private readonly Dictionary<string, string> playerPrefsGroup = new();
 		private readonly Dictionary<Type, IModel> modelGroup = new();
 
 		public MvpContainer()
@@ -29,32 +23,11 @@ namespace Redbean.Singleton
 
 			foreach (var model in models)
 				modelGroup.TryAdd(model.GetType(), model);
-
-#region PlayerPrefs
-
-			if (PlayerPrefs.HasKey(PLAYER_PREFS_KEY))
-			{
-				var dataDecrypt = PlayerPrefs.GetString(PLAYER_PREFS_KEY).Decryption();
-				var dataGroups = JsonConvert.DeserializeObject<Dictionary<string, string>>(dataDecrypt);
-				foreach (var dataGroup in dataGroups)
-				{
-					var key = Assembly.Load("Assembly-CSharp").GetTypes().FirstOrDefault(_ => _.FullName == dataGroup.Key);
-					var value = JsonConvert.DeserializeObject(dataGroup.Value, key);
-
-					if (value is IModel model)
-						modelGroup[key] = model;
-				}
-
-				playerPrefsGroup = dataGroups;
-			}
-
-#endregion
 		}
 
 		public void Dispose()
 		{
 			modelGroup.Clear();
-			playerPrefsGroup.Clear();
 		}
 
 		/// <summary>
@@ -75,45 +48,15 @@ namespace Redbean.Singleton
 		/// <summary>
 		/// 모델 재정의
 		/// </summary>
-		public T Override<T>(T value, bool isPlayerPrefs = false) where T : IModel
+		public T Override<T>(T value) where T : IModel
 		{
 			var targetFields = modelGroup[value.GetType()].GetType().GetFields(BindingFlags.Instance | BindingFlags.Public).ToArray();
 			var copyFields = value.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public).ToArray();
 			
 			for (var i = 0; i < targetFields.Length; i++)
 				targetFields[i].SetValue(modelGroup[value.GetType()], copyFields[i].GetValue(value));
-
-			if (isPlayerPrefs)
-				Save(value);
 			
 			return value;
 		}
-		
-#region PlayerPrefs
-		
-		/// <summary>
-		/// 로컬 데이터 저장 및 퍼블리싱
-		/// </summary>
-		public T Save<T>(T value)
-		{
-			playerPrefsGroup[typeof(T).FullName] = JsonConvert.SerializeObject(value);
-			
-			var encryptValue = JsonConvert.SerializeObject(playerPrefsGroup).Encryption();
-			PlayerPrefs.SetString(PLAYER_PREFS_KEY, encryptValue);
-			
-			return value;
-		}
-		
-		/// <summary>
-		/// 로컬 데이터 호출
-		/// </summary>
-		public T Load<T>()
-		{
-			return playerPrefsGroup.TryGetValue(typeof(T).FullName, out var value) 
-				? JsonConvert.DeserializeObject<T>(value) 
-				: default;
-		}
-		
-#endregion
 	}
 }
